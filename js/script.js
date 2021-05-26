cf7ef8e182688c1e0392de5e10939f97a3027482
leonardo <sponzal4@gmail.com>
2021-05-26 15:12:49 +0200

    java

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ var testo = prompt ('inserisci il nome');
 console.log(testo);
 var numero = parseInt(prompt('inserisci età'));
 console.log(numero);
-var colore = parseInt(prompt('inserisci colore preferito'));
-console.log(colore);
+var color = parseInt(prompt('inserisci colore preferito'));
+console.log(color);
